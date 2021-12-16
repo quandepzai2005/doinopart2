14133e537d9b0272989f6e5cc24996d08a97957a
quandepzai2005 <90812762+quandepzai2005@users.noreply.github.com>
2021-12-16 22:28:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào thg ml',
+    introDesc: `T có điều này muốn hỏi m, bắt buộc phải trả lời thật lòng !!!`,
+    btnIntro: 'oke',
+    title: 'DCM trả t 5k',
+    desc: 'không trả lời hoặc thoát ra, t lấy lãi gấp đôi ',
+    btnYes: 'oke tí t trả',
+    btnNo: 'khi nào có t trả',
+    question:'nếu không trả thì sao ???',
+    btnReply: 'Gửi lẹ t còn mua kẹo',
+    reply: 't sẽ trả lãi gấp đôi, rồi chép bài cho m ',
+    mess: 'nhớ đấy lẹ lẹ ko t đấm',
+    messDesc: 'còn giờ thì chờ gì nữa trả nhanh cmm lên',
+    btnAccept: 'ừa ừa ừa',
+    messLink: 'https://www.messenger.com/t/100014607586860' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
